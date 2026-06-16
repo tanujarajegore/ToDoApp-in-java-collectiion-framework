8bc83ada174d2593cb271e77fe3197d7ec9127f0
Tanuja Rajegore <tanujarajegore7@gmail.com>
2026-06-16 14:24:17 +0530

    Update ToDoApp.java

--- ToDoApp.java
+++ ToDoApp.java
@@ -72,7 +72,7 @@ class TodoApp {
 
         do {
             System.out.println("===== ToDoApp =====");
-            System.out.println("\n1. Add Task");
+            System.out.println("1. Add Task");
             System.out.println("2. View Task");
             System.out.println("3. Complete Task");
             System.out.println("4. Delete Task");
